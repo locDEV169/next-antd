67f221b307dac53d04c3d317aa5c17a16e11c9c9
loc.nguyen <loc.nguyen@napaglobal.com>
2023-01-16 10:52:14 +0700

    connect metamark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,22 @@
+import { configureChains } from '@wagmi/core';
+import {
+  arbitrum,
+  arbitrumGoerli,
+  avalanche,
+  avalancheFuji,
+  bsc,
+  bscTestnet,
+  fantom,
+  fantomTestnet,
+  foundry,
+  goerli,
+  mainnet,
+  optimism,
+  optimismGoerli,
+  polygon,
+  polygonMumbai,
+  sepolia,
+} from '@wagmi/core/chains';
 import { FC, useState } from 'react';
 import 'styles/globals.less';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
@@ -9,12 +28,43 @@ import { AppProps } from 'next/app';
 import store from 'store';
 import { useRouteLoading } from 'hooks/useRouteLoading';
 import Layout from 'Layout';
+import { createClient, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { SessionProvider } from 'next-auth/react';
 
 const queryClientOption = {
   defaultOptions: {
     queries: { refetchOnWindowFocus: false, retry: false, staleTime: 1000 * 5 },
   },
 };
+
+const { provider, webSocketProvider } = configureChains(
+  [
+    arbitrum,
+    arbitrumGoerli,
+    avalanche,
+    avalancheFuji,
+    bsc,
+    bscTestnet,
+    fantom,
+    fantomTestnet,
+    foundry,
+    goerli,
+    mainnet,
+    optimism,
+    optimismGoerli,
+    polygon,
+    polygonMumbai,
+    sepolia,
+  ],
+  [publicProvider()]
+);
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+});
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient(queryClientOption));
@@ -29,9 +79,13 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
+            <WagmiConfig client={client}>
+              <SessionProvider session={pageProps.session} refetchInterval={0}>
+                <Layout>
+                  <Component {...pageProps} />
+                </Layout>
+              </SessionProvider>
+            </WagmiConfig>
             <ReactQueryDevtools initialIsOpen={false} />
           </Hydrate>
         </QueryClientProvider>
