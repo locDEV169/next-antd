c2d1b2f115dfff6d70ad1f286ef651d7f4b9d5ce
loc.nguyen <loc.nguyen@napaglobal.com>
2023-02-02 17:13:17 +0700

    Ui homepage admin site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Space } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card, Space, Tag } from 'antd';
 import clsx from 'clsx';
 import { Button } from 'components/Button';
 import { useAppDispatch, useAppSelector } from 'hooks';
@@ -6,9 +7,15 @@ import type { NextPage } from 'next';
 import { getHelloMessage, setHelloMessage } from 'store/ducks/hello/slide';
 import styles from './styles.module.less';
 
+const { Meta } = Card;
+
 const Home: NextPage = () => {
   const dispatch = useAppDispatch();
   const helloMessage = useAppSelector(getHelloMessage);
+
+  const listCard = ['a', 'b', 'c', 'a', 'b', 'c'];
+
+  console.log(listCard.length);
 
   return (
     <div className={clsx(styles.main, 'container')}>
@@ -18,6 +25,67 @@ const Home: NextPage = () => {
           <Button onClick={() => dispatch(setHelloMessage('Hello world'))}>Say hello</Button>
           {helloMessage}
         </Space>
+        <div className={styles.contentCard}>
+          {listCard.map((item, index) => (
+            <div className={styles.card}>
+              <Card
+                style={{ width: 500, margin: '20px' }}
+                cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                // actions={[
+                //   <SettingOutlined key="setting" />,
+                //   <EditOutlined key="edit" />,
+                //   <EllipsisOutlined key="ellipsis" />,
+                // ]}
+                key={index}
+              >
+                <Meta
+                  // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                  title="City XXX"
+                />
+                <div className={styles.description}>
+                  <div className={styles.title}>XX Forest</div>
+                  <div style={{ display: 'flex' }}>
+                    <Tag color="default" className={styles.tags} style={{ marginLeft: '0px' }}>
+                      co2
+                    </Tag>
+                    <Tag color="default" className={styles.tags}>
+                      water
+                    </Tag>
+                    <Tag color="default" className={styles.tags}>
+                      microorgainsm
+                    </Tag>
+                  </div>
+                </div>
+                <Meta
+                  // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                  title="City XXX"
+                  // description="This is the description"
+                />
+                <div className={styles.description}>
+                  <div className={styles.title}>XX Forest</div>
+                </div>
+                <Meta
+                  // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                  title="City XXX"
+                />
+                <div className={styles.description}>
+                  <div className={styles.title}>XX Forest</div>
+                  <div style={{ display: 'flex' }}>
+                    <Tag color="default" className={styles.tags} style={{ marginLeft: '0px' }}>
+                      co2
+                    </Tag>
+                    <Tag color="default" className={styles.tags}>
+                      water
+                    </Tag>
+                    <Tag color="default" className={styles.tags}>
+                      microorgainsm
+                    </Tag>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          ))}
+        </div>
       </>
     </div>
   );
