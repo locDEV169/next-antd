213cab0b261f6ca6e91c7e5315d30545a7d9d940
loc.nguyen <loc.nguyen@napaglobal.com>
2023-02-09 17:09:53 +0700

    ui natural page

--- src/pages/natural-resources/natural-resources.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import type { NextPage } from 'next';
-
-const NaturalResource: NextPage = () => {
-  return <div>NaturalResource page</div>;
-};
-
-export default NaturalResource;
