bf6d70a13a4a34b8c529dcb893bab6edfffb4977
locDEV169 <loc16910@gmail.com>
2023-02-12 22:38:30 +0700

    UI enviroment page

--- src/pages/environmental-values/environmental-values.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import type { NextPage } from 'next';
-
-const EnvironmentalValues: NextPage = () => {
-  return <div>Environmental Values page</div>;
-};
-
-export default EnvironmentalValues;
