a05639b7cbdaead099e54c7ed24b9e268645fd7e
loc.nguyen <loc.nguyen@napaglobal.com>
2023-02-23 11:46:58 +0700

    handler error for call api failed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,13 +1,22 @@
 /* eslint-disable no-restricted-syntax */
+import { notification } from 'antd';
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export const authRequest = axios.create({
   baseURL: '/',
   headers: {
-    "Content-Type": "application/json",
-    Authorization: "",
+    'Content-Type': 'application/json',
+    Authorization: '',
   },
 });
+
+const openNotificationWarning = (message: string, description: string) => {
+  return notification.warning({
+    message: message,
+    description: description,
+    placement: 'bottomRight',
+  });
+};
 
 const handleSuccess = (res: AxiosResponse) => {
   // if (res.request) return res.data;
@@ -17,6 +26,29 @@ const handleSuccess = (res: AxiosResponse) => {
 const handleError = (err: AxiosError) => {
   // const data = err?.response?.data;
   const data = err?.response;
+
+  if (data?.status === 401 || data?.data.statusCode === 401) {
+    //message.warning('Please login again!');
+    // store.dispatch(actionDisconnectWallet());
+    const message = 'Notification error 401';
+    const description =
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.';
+    openNotificationWarning(message, description);
+  }
+  if (data?.status === 404 || data?.data.statusCode === 404) {
+    const message = 'Notification error 404';
+    const description =
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.';
+    openNotificationWarning(message, description);
+  }
+
+  if (data?.status === 500 || data?.data.statusCode === 500) {
+    const message = 'Notification error 500';
+    const description =
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.';
+    openNotificationWarning(message, description);
+  }
+
   return Promise.reject(data);
 };
 
--- src/api/post-request/request.ts
+++ src/api/post-request/request.ts
@@ -1,3 +1,6 @@
+import { notification } from 'antd';
+import { authRequest } from 'api/axios';
+
 export const postRequest = async (url: string, params: any): Promise<any> => {
   const domain = 'http://localhost:5000';
 
@@ -10,13 +13,52 @@ export const postRequest = async (url: string, params: any): Promise<any> => {
       },
       body: JSON.stringify(params),
     });
+    if (response.status === 400 || response.status === 401 || response.status === 404 || response.status === 500) {
+      handleError(response.status);
+    }
+
     return response.json();
-    // const { data } = await authRequest.post(`${domain}/${url}`, params);
-
-    // console.log('postRequest', response);
-
-    // return response;
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
   }
+  // const { data } = await authRequest.post(`${domain}/${url}`, params);
+
+  // console.log('postRequest', data);
+
+  // return data;
+};
+
+//handle error for post request
+const handleError = (status: number) => {
+  const data: number = status;
+
+  if (data === 401) {
+    const message = 'Notification error 401';
+    const description =
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.';
+    openNotificationWarning(message, description);
+  }
+  if (data === 404) {
+    const message = 'Notification error 404';
+    const description =
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.';
+    openNotificationWarning(message, description);
+  }
+
+  if (data === 500) {
+    const message = 'Notification error 500';
+    const description =
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.';
+    openNotificationWarning(message, description);
+  }
+
+  return Promise.reject(data);
+};
+
+const openNotificationWarning = (message: string, description: string) => {
+  return notification.warning({
+    message: message,
+    description: description,
+    placement: 'bottomRight',
+  });
 };
